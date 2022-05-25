192b41543e8648062a925fed1c304161a533cadd
Rodrigo <rodrigdsoto@gmail.com>
2022-05-25 20:52:32 -0300

    Signed-off-by: Rodrigo <rodrigdsoto@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sidebar4';
+export class AppComponent implements OnInit{
+title = 'sidebar4';
+expanded=true;
+  ngOnInit(): void {
+  this.collapse();
+    
+  }
+  collapse(){
+  console.log("submenu");
+  
+    const btnsubmenu=document.querySelector(".container-submenu");
+    const btn= document.getElementsByClassName("container-submenu");
+     
+    for(let i=0;i<btn.length;i++){
+      btn[i].addEventListener("click",()=>{
+        let submenu=btn[i].children.length;
+        let sub=btn[i].children[2];
+        sub.classList.toggle("collapse");
+        console.log(submenu);
+        console.log(sub);
+        
+        
+      });
+    }
+    
+   
+  }
+  
 }
