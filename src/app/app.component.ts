d9ea2a124cba1ad18ab5d2bc042b1eec8f9d22b6
Rodrigo <rodrigdsoto@gmail.com>
2022-05-26 13:26:26 -0300

    Signed-off-by: Rodrigo <rodrigdsoto@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,17 @@ export class AppComponent implements OnInit{
 title = 'sidebar4';
 expanded=true;
   ngOnInit(): void {
-  this.collapse();
+    this.collapse();
+    this.menu_control();
+    this.selected();
     
+  }
+  menu_control(){
+    const btnmenu=document.getElementById("btnMenu");
+    const sidebar=document.querySelector(".sidebar");
+    btnmenu?.addEventListener("click",()=>{
+        sidebar?.classList.toggle("expand");
+    });
   }
   collapse(){
   console.log("submenu");
@@ -22,15 +31,21 @@ expanded=true;
       btn[i].addEventListener("click",()=>{
         let submenu=btn[i].children.length;
         let sub=btn[i].children[2];
-        sub.classList.toggle("collapse");
-        console.log(submenu);
-        console.log(sub);
-        
-        
+        sub.classList.toggle("collapse");        
       });
-    }
-    
-   
+    }     
   }
+  selected(){
+    const item_menu=document.querySelectorAll(".item-menu a");
+    for(let i=0;i<item_menu.length;i++){
+      item_menu[i].addEventListener("click",()=>{
+        for(let i=0;i<item_menu.length;i++){
+          item_menu[i].classList.remove("selected");
+        }        
+        item_menu[i].classList.add("selected");
+      }); 
+    }    
+  }
+  
   
 }
